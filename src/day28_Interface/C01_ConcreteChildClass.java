2326a7ced8a9b9d40b69bde2aeeb9a0aed2cbac5
Pinzack <cahitblk@hotmail.com>
2023-05-12 13:59:32 +0300

    28.Ders Sonu

---
+++ src/day28_Interface/C01_ConcreteChildClass.java
@@ -0,0 +1,29 @@
+package day28_Interface;
+
+import day27_pollymorphism_abstractClasses.C07_AbstractP;
+
+public class C01_ConcreteChildClass extends C07_AbstractP {
+
+    /* Abstrack parent(lar)dan sonra gelen
+    ILK Concrete Child class
+    Parent abstract class(lar)dan concrete'lestirilmemiş olan
+    tum abstract method'lari
+    İMPLEMEMT ETMEK ZORUNDADİR
+
+
+    concrete olan methodlar ise opsiyoneldir
+        isterseniz override edebilirsiniz
+
+     */
+
+
+    @Override
+    protected void method2() {
+
+    }
+
+    @Override
+    protected void method5() {
+
+    }
+}
